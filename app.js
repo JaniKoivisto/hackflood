679cb49513b3f8eba2d0c9bfc302a46811e5a3a2
JaniKoivisto <koivisto.jani@live.fi>
2018-01-28 13:33:17 +0200

    Styling

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require('express')
 const app = express()
 
-var config = require('./config.json');
-
-const pg = require('pg');
-var conString = "pg://" + config.username + ":" + config.password + "@postgresql-test.cx31dnxxuh63.us-east-2.rds.amazonaws.com:5432/" + config.dbname;
-var client = new pg.Client(conString);
-client.connect();
-
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
